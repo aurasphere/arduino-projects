ea109c5149a577a40ef54d3cd862887a5d291be7
Donato Rimenti <donatohan.rimenti@gmail.com>
2021-06-02 00:50:43 +0200

    Added sonar files.

---
+++ sonar/README.md
@@ -0,0 +1,12 @@
+# Sonar
+
+Sonar-like program that bips faster when objects gets closer to the sensor.
+
+## Components
+ - HC-SR04 Ultrasonic Sensor
+ - Active Buzzer
+ - Arduino Nano
+ 
+## Schematics
+
+![Schematics](sonar.png)
--- sonar/sonar.ino
+++ sonar/sonar.ino
@@ -1,12 +1,12 @@
-//www.elegoo.com
-//2016.12.08
 #include "SR04.h"
-
 #include "pitches.h"
+
 #define TRIG_PIN 12
 #define ECHO_PIN 11
+#define BUZZER_PIN 7
+#define SR04_MAX_RANGE_CM 4000
+
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-long a;
 
 void setup() {
    Serial.begin(9600);
@@ -14,19 +14,15 @@ void setup() {
 }
 
 void loop() {
-   a=sr04.Distance();
-
-  int note;
-   if (a < 30) {
-      if (a > 25) note = NOTE_C5;
-      else if (a > 20) note = NOTE_D5;
-      else if (a > 15) note = NOTE_E5;
-      else if (a > 10) note = NOTE_F5;
-      else if (a > 5) note = NOTE_G5;
-      else note = NOTE_A5;
-      tone(7, note);
-    } else noTone(7);
-   Serial.print(a);
-   Serial.println("cm");
+   long distance = sr04.Distance();
+   
+   // Plays a sound according to the distance.
+   tone(BUZZER_PIN, NOTE_C5);
+   delay(250);
+   noTone(BUZZER_PIN);
+   delay(250 + (distance * 25));
   
+   // Prints the distance
+   Serial.print(distance);
+   Serial.println("cm");
 }
